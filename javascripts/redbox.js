2bd149ec767b514c02206adc85126864ae6f16d9
Steven Chanin <steven_chanin@alum.mit.edu>
2010-03-02 13:02:05 -0800

    slide an iFrame under the popup when viewed in IE6 IE6 has a problem showing overlays when the underlying document contains selects.  It has to do with windowed and windowless elements (http://www.faqts.com/knowledge_base/view.phtml/aid/26208)
    One of the fixes is to slide an iFrame under your
    popup to cover up the selects

--- javascripts/redbox.js
+++ javascripts/redbox.js
@@ -1,6 +1,32 @@
 
 var RedBox = {
 
+  isIE6: null,
+  
+  usingIE6: function ()
+  {
+    if (this.isIE6 == null) {
+      this.isIE6 = (this.getInternetExplorerVersion() == 16);
+    }
+
+    return(this.isIE6);
+  },
+  
+  getInternetExplorerVersion: function()
+  // Returns the version of Internet Explorer or a -1
+  // (indicating the use of another browser).
+  {    
+    var rv = -1; // Return value assumes failure.
+    if (navigator.appName == 'Microsoft Internet Explorer')
+    {
+      var ua = navigator.userAgent;
+      var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+      if (re.exec(ua) != null)
+        rv = parseFloat( RegExp.$1 );
+    }
+    return rv;
+  },  
+
   showInline: function(id)
   {
     this.showOverlay();
@@ -43,10 +69,17 @@ var RedBox = {
     {
       new Insertion.Bottom(document.body, '<div id="RB_redbox" align="center"><div id="RB_window" style="display: none;"></div><div id="RB_overlay" style="display: none;"></div></div>');      
     }
-    new Insertion.Top('RB_overlay', '<div id="RB_loading" style="display: none"></div>');  
+    
+    if (this.usingIE6()) {
+      new Insertion.Top('RB_window', '<iframe id="RB_iframe" frameborder=1 style="display:none;"></iframe>');
+    }
+    new Insertion.Top('RB_overlay', '<div id="RB_loading" style="display: none"></div>');
 
     this.setOverlaySize();
-    new Effect.Appear('RB_overlay', {duration: 0.4, to: 0.6, queue: 'end'});
+    new Effect.Appear('RB_overlay', {duration: 0.4, to: 0.4, queue: 'end'});
+    if (this.usingIE6()) {
+      new Effect.Appear('RB_iframe');
+    }
   },
 
   setOverlaySize: function()
@@ -78,7 +111,14 @@ var RedBox = {
     var height = dimensions.height;        
     
     $("RB_window").style['left'] = ((pagesize[0] - width)/2) + "px";
-    $("RB_window").style['top'] = ((pagesize[1] - height)/2) + "px";
+    $("RB_window").style['top'] = ((pagesize[1] - height)/4) + "px";
+    
+    if (this.usingIE6()) {
+      $("RB_iframe").style['position'] = "absolute";
+      $("RB_iframe").style['width'] = width + "px";
+      $("RB_iframe").style['height'] = height + "px";
+      $("RB_iframe").style['zIndex'] = -1;
+    }
   },
 
 
